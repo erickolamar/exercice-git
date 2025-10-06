d1a7b90653030a1def56589ab09b3d9dc844dfc2
Joachim2371907 <joachimjovinwabo@gmail.com>
2025-10-06 16:15:23 +0100

    new

---
+++ addition.py
@@ -0,0 +1,10 @@
+def addition(a, b):
+    """Fonction qui retourne la somme de deux nombres"""
+    return a + b
+
+# Exemple d'utilisation
+nombre1 = 10
+nombre2 = 5
+
+resultat = addition(nombre1, nombre2)
+print("La somme de", nombre1, "et", nombre2, "est :", resultat)
